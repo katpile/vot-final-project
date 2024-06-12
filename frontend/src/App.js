f7145b10ba94881b3bd85a4171221cbf6b39e402
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2024-06-12 20:13:36 +0300

    keyclock register completed

---
+++ frontend/src/App.js
@@ -0,0 +1,59 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Keycloak from 'keycloak-js';
+
+/*
+  Init Options
+*/
+let initOptions = {
+  url: 'http://localhost:8080/',
+  realm: 'myrealm',
+  clientId: 'frontend',
+}
+
+let kc = new Keycloak(initOptions);
+
+kc.init({
+  onLoad: 'login-required', // Supported values: 'check-sso' , 'login-required'
+  checkLoginIframe: true,
+}).then((auth) => {
+  if (!auth) {
+  } else {
+    /* Remove below logs if you are using this on production */
+    console.info("Authenticated");
+    console.log('auth', auth)
+    console.log('Keycloak', kc)
+    console.log('Access Token', kc.token)
+
+    kc.onTokenExpired = () => {
+      console.log('token expired')
+    }
+  }
+}, () => {
+  /* Notify the user if necessary */
+  console.error("Authentication Failed");
+});
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
