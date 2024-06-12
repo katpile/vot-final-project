f618c2f51bfc9803bf97f88c8fad25a682d7ffb7
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2024-06-12 11:54:45 +0300

    docker-compose working

--- docker_config/init_mariadb.sql
+++ docker_config/init_mariadb.sql
@@ -1,3 +1,12 @@
+-- Create root user for any host with mysql_native_password
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'rootpassword';
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'rootpassword';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+-- Create maxscale_user with mysql_native_password
+CREATE USER IF NOT EXISTS 'maxscale_user'@'%' IDENTIFIED BY '12345678';
+ALTER USER 'maxscale_user'@'%' IDENTIFIED WITH mysql_native_password BY '12345678';
 GRANT ALL PRIVILEGES ON *.* TO 'maxscale_user'@'%';
 FLUSH PRIVILEGES;
 
@@ -7,9 +16,9 @@ USE people;
 DROP TABLE IF EXISTS people;
 
 CREATE TABLE people (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        name VARCHAR(255) NOT NULL,
-        school VARCHAR(255) NOT NULL
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        name VARCHAR(255) NOT NULL,
+                        school VARCHAR(255) NOT NULL
 );
 
 INSERT INTO people (name, school) VALUES ('Niki', '32 language school');
